876b7cb9da9381ecbfc657c5703bf02c4e4931d9
My Vo <vodanguyenmy@gmail.com>
2020-11-14 12:29:01 +0700

    my update object, pic

---
+++ app/src/main/java/edu/fu/rentcarnavbar/Object/color.java
@@ -0,0 +1,19 @@
+package edu.fu.rentcarnavbar.Object;
+
+public class color {
+    int color_id;
+    String color;
+
+    public color(int color_id, String color) {
+        this.color_id = color_id;
+        this.color = color;
+    }
+
+    public int getColor_id() {
+        return color_id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
