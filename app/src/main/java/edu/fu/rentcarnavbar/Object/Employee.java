a7a675804b9fbf0a75c0db5c75e738528148f179
Minh Tân <58105178+PhamNhatMinhTan@users.noreply.github.com>
2020-11-16 21:16:05 +0700

    Create object Employee

---
+++ app/src/main/java/edu/fu/rentcarnavbar/Object/Employee.java
@@ -0,0 +1,82 @@
+package edu.fu.rentcarnavbar.Object;
+
+public class Employee {
+
+    //declare variables
+    private int id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String address;
+    private int status;
+
+    public Employee(int id, String username, String password, String name, String email,
+                    String address, int status) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.status = status;
+    }
+
+    public Employee() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
